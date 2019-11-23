8f28d38d4e9c7cc3f10b3fcd1f90d5a1237e75ed
Parasuram, Prakash (Prakash) <prakash@nationwide.com>
2019-11-23 16:53:05 -0500

    fourth commit

---
+++ src/cscc/edu/MainLater.java
@@ -0,0 +1,11 @@
+package cscc.edu;
+
+public class MainLater {
+    //public static void main(String[] args) {
+    public void MainLater() {
+        int[] a = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+        a = Functions.mergeSort(a);
+        // int idx = Functions.binarySearch(a, 4);
+    }
+    // }
+}
